11c731c004fa751afb866d4506dc703ad98ff0b6
leticiaoliveira25 <oliveira.leticia25@escola.pr.gov.br>
2025-05-22 12:35:19 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,17 @@
+criaCartao(
+    'Sociologia',
+    'Qual o nome do sociólogo que defende a luta de classes para moldar a sociologia?',
+    'Karl Marx'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da França?',
+    'A capital da França é Paris'
+)
+
+criaCartao(
+    'BIOLOGIA',
+    'Qua é a célula da flor?',
+    'A célula eucrionte vegetal'
+)
